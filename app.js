117c8ebc5ce375343900102ae6729138f53326c5
litaglk <talitaglk.trad@gmail.com>
2024-09-06 12:15:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    //Obtém o campo de pesquisa
+    let campoPesquisa = document.getElementById 
+    ("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Digite o nome de uma obra ou estudo</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados formatados em HTML
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada item de dados e constrói o HTML correspondente
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descrição-meta">${dado.descricao}
+                </p>
+                <a href="${dado.link}" target="_blank">Saiba Mais</a>
+            </div>
+        `;
+        }
+}
+
+if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>"
+}
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "A Redoma de Vidro",
+        descricao: "A Redoma de Vidro é um romance quase autobiográfico de Sylvia Plath que nos convida a uma profunda jornada pela mente de uma jovem mulher em crescimento.",
+        link: "https://pt.wikipedia.org/wiki/Sylvia_Plath",
+        tags: "biografia cidade feminismo"
+      },
+      {
+        titulo: "Ariel",
+        descricao: "Ariel é um dos livros de poesia mais aclamados de Sylvia Plath, conhecido por sua intensidade emocional e exploração de temas como a morte, a loucura e a maternidade.",
+        link: "https://pt.wikipedia.org/wiki/Ariel_(livro)",
+        tags: "poema análise"
+      },
+      {
+        titulo: "Os Diários de Sylvia Plath",
+        descricao: "Os Diários de Sylvia Plath oferecem um olhar íntimo e revelador sobre a vida da poetisa, desde sua infância até os anos seus últimos dias de vida",
+        link: "https://pt.wikipedia.org/wiki/Os_Diários_de_Sylvia_Plath",
+        tags: "casamento relação obra"
+      }
+    ];
