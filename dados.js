c0e7f0f96076f50054e74887506d858777619ada
litaglk <talitaglk.trad@gmail.com>
2024-10-25 22:19:09 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -22,5 +22,17 @@ let dados = [
         descricao: "Johnny Panic e a Bíblia dos Sonhos é uma das obras mais enigmáticas e intensas da autora. Publicado postumamente, o livro é uma coletânea de poemas que nos leva a um labirinto de emoções, medos e desejos que habitavam sua mente",
         link: "https://en.wikipedia.org/wiki/Johnny_Panic_and_the_Bible_of_Dreams",
         tags: "sonhos psicanálise conto"
-    }
+    },
+      {
+      titulo: "Análise das Múltiplas Identidades de Sylvia Plath Post Mortem",
+      descricao: "Estudo realizado por KK, com orientação de JCR, sobre como a obra e a pessoa de Sylvia Plath passou a ser retratada após sua morte, em 1963.",
+      link: "https://www.puc-rio.br/ensinopesq/ccpg/pibic/relatorio_resumo2019/download/relatorios/CCS/COM/COM-Karen%20Krieger.pdf",
+      tags: "análise biogafia obra póstuma"
+    },
+    {
+      titulo: "Sylvia Plath, entre a escrita e o sangue: O trágico como potência do inefável da vida",
+      descricao: "Estudo realizado por BVLS, com orientação de MCAA, sobre a abordagem da morte nas obras da autora",
+      link: "https://repositorio.ufmg.br/bitstream/1843/LETR-AP5GJ4/1/disserta__o___beatriz_viana_lopes_saltarelli.pdf",
+      tags: "morte niilismo trágico"
+    },
     ];
